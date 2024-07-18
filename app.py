90bcda9f8b863b7fcf56294c37de282325559456
Suyash Chaklan <suyashchaklanstart@gmail.com>
2024-07-18 10:14:25 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+CLUBS = [{
+    'id': 1,
+    'name': 'Avyaan',
+    'arts': 'Music',
+    'type': 'Cultural'
+}, {
+    'id': 2,
+    'name': 'Enigma',
+    'arts': 'Dance',
+    'type': 'Cultural'
+}, {
+    'id': 3,
+    'name': 'Khalas',
+    'arts': 'Dance',
+    'type': 'Cultural'
+}, {
+    'id': 4,
+    'name': 'Atrangi',
+    'arts': 'Art and Poetry',
+    'type': 'Cultural'
+}, {
+    'id': 5,
+    'name': 'Adaa',
+    'arts': 'Fashion',
+    'type': 'Cultural'
+}]
+
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', clubs=CLUBS)
 
 
 if __name__ == "__main__":
